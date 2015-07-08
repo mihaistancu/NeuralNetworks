2113ebdbb784975ce636196e6ff6377154ca7105
mihaistancu <stancu_meehigh@yahoo.com>
2015-07-08 16:12:01 +0300

    initialized weights and biases

--- NeuralNetwork/NeuralNetwork.cs
+++ NeuralNetwork/NeuralNetwork.cs
@@ -1,7 +1,47 @@
-﻿namespace NeuralNetwork
+﻿using System;
+
+namespace NeuralNetwork
 {
     public class NeuralNetwork
     {
+        readonly Random random = new Random();
 
+        private double[][][] weights;
+        private double[][] biases;
+
+        public NeuralNetwork(int[] layerSizes)
+        {
+            weights = new double[layerSizes.Length - 1][][];
+
+            for (int i = 0; i < layerSizes.Length - 1; i++)
+            {
+                weights[i] = GetRandomMatrix(layerSizes[i], layerSizes[i + 1]);
+                biases[i] = GetRandomVector(layerSizes[i + 1]);
+            }
+        }
+
+        private double[] GetRandomVector(int n)
+        {
+            var vector = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vector[i] = random.NextDouble();
+            }
+
+            return vector;
+        }
+
+        private double[][] GetRandomMatrix(int m, int n)
+        {
+            var matrix = new double[m][];
+
+            for (int i = 0; i < m; i++)
+            {
+                matrix[i] = GetRandomVector(n);
+            }
+
+            return matrix;
+        }
     }
 }
